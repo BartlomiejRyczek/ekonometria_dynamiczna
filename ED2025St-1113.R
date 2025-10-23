687c6a90c3c191947de3a8bb95fb83dadf65a421
Bartłomiej Ryczek <114954885+BartlomiejRyczek@users.noreply.github.com>
2025-10-23 12:01:27 +0200

    Add files via upload

---
+++ ED2025St-1113.R
@@ -0,0 +1,93 @@
+###### pakiety ######
+
+if(!require(tseries)){
+  install.packages('tseries')
+  library(tseries)
+}
+
+if(!require(plotrix)){
+  install.packages('plotrix')
+  library(plotrix)
+}
+
+if(!require(zoo)){
+  install.packages('zoo')
+  library(zoo)
+}
+
+if(!require(vars)){
+  install.packages('vars')
+  library(vars)
+}
+
+
+#####  Katalog roboczy #####
+
+
+#####  Import danych #####
+#Dane kwartalne z okresu 1947:1 - 2021:2
+#Y - produkt krajowy brutto
+#C - prywatne wydatki konsumpcyjne
+#I - prywatne inwestycje krajowe brutto
+
+  
+##### wczytanie danych i wstepna analiza #####
+
+#wczytanie danych
+
+#wyswietla poczatek danych
+#wyswietla koniec danych 
+
+#liczba analizowanych zmiennych
+
+#liczba zebranych obserwacji
+
+#nazwy zmiennych
+
+
+#####  Przekształcanie zmiennych #####
+
+#logarytm naturalny
+
+#zapis danych w postaci macierzy
+
+#zapis danych w postaci szeregu czasowego
+
+
+#####  Wykresy i wstępna analiza poziomów #####
+
+#laczny wykres calego szeregu
+
+
+#laczny wykres poziomow calego szeregu z trendem liniowym
+
+#Analiza poziomów
+
+#liczba opóznien do ACF i PACF
+
+#szereg, acf, pacf
+par(mfrow=c(1,1))#jeden wykres
+par(mfrow=c(n, 3))#podzial obszaru wykresu na n wierszy i 3 kolumny 
+
+#zapis do pliku pdf
+#wymiary
+pdf_h = 8.75
+pdf_w = 12.71
+
+#otwieramy plik
+pdf(file = "poziomy.pdf", width = pdf_w, height = pdf_h)
+par(mfrow=c(1,1))
+par(mfrow=c(n, 3))
+
+#wykres
+
+dev.off()#zamykamy plik
+
+#To co wyzej, tylko z zapisem do pliku jpeg
+jpeg(file = "poziomy.jpeg")
+par(mfrow=c(1,1))
+par(mfrow=c(n, 3))
+#wykres
+
+dev.off()
+
